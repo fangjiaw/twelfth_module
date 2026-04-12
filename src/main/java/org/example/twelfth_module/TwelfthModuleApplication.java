20c5fe18ca08f3fe804578b5544e83a9c0306637
“fangjiaw” <1906233698@qq.com>
2026-04-12 13:01:08 +0800

    Initial commit

---
+++ src/main/java/org/example/twelfth_module/TwelfthModuleApplication.java
@@ -0,0 +1,13 @@
+package org.example.twelfth_module;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwelfthModuleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TwelfthModuleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/twelfth_module/TwelfthModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.twelfth_module;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwelfthModuleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
